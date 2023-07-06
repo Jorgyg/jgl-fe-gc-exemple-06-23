3b3b57423531e3b374e25eced820a99b53412d87
Jorgyg <jgraslorenzo@gmail.com>
2023-07-06 17:42:53 +0200

    Calculadora

--- Angular/calculadora/src/app/app.component.ts
+++ Angular/calculadora/src/app/app.component.ts
@@ -7,50 +7,69 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculadora';
-
+  numArray: number[] = [];
   numC: string = '';
-
+  num: string = '';
+  op: string[] = [];
   disp(num: string) {
+    this.num += num;
     this.numC += num;
   } 
 
   delete(){
+    this.num = '';
     this.numC = '';
+    this.numArray = [];
+    this.op = [];
   }
 
+  operator(op: string){
+    this.op.push(op);
+    this.numC += op;
+    this.numArray.push(+this.num);
+    this.num = '';
+  }
+  
   calculate() {
-    
-    let arrayNumbers = [];
-    let finalNum = 0;
-    arrayNumbers = this.numC.split(" ");
-
-    for (let i = 0; i < arrayNumbers.length; i++) {
-      if(i == 0){
-        finalNum = +arrayNumbers[i];
-        i++;
-      }
-        let num2 = +arrayNumbers[i+1];
-        switch (arrayNumbers[i] as any) {
-          case '+':
-            finalNum+= num2;
-            break;
-          case '-':
-            finalNum-= num2;
-            break;
-          case 'X':
-            finalNum = finalNum * num2;
-            break;
-          case '/':
-            finalNum+= finalNum / num2;
-            break;
-          default:
-            break;
+    this.numArray.push(+this.num);
+    this.num = '';
+  
+    // Primero multiplicamos y dividimos
+    for (let i = 0; i < this.op.length; i++) {
+      if (this.op[i] === 'X') {
+        this.numArray[i] = this.numArray[i] * this.numArray[i + 1];
+        this.numArray.splice(i + 1, 1);
+        this.op.splice(i, 1);
+        i--; // Restamos i porque borramos un operador
+      } else if (this.op[i] == '/') {
+        if (this.numArray[i + 1] == 0) {
+          // Divison por cero
+          this.numC = 'Syntax error';
+          return;
         }
-        i++;      
+        this.numArray[i] = this.numArray[i] / this.numArray[i + 1];
+        this.numArray.splice(i + 1, 1);
+        this.op.splice(i, 1);
+        i--; // 
+      }
     }
-    
-    this.numC = finalNum.toString();
-
+  
+    let totalNumber = this.numArray[0];
+  
+    //Sumas y restas
+    for (let i = 0; i < this.op.length; i++) {
+      if (this.op[i] == '+') {
+        totalNumber += this.numArray[i + 1];
+      } else {
+        totalNumber -= this.numArray[i + 1];
+      }
+    }
+  
+    this.numC = totalNumber.toString();
+    this.num = this.numC;
+    this.numArray = [];
+    this.numArray.push(+this.numC);
+    this.op = [];
   }
-
+  
 }
--- DOM/script/script.js
+++ DOM/script/script.js
@@ -1,15 +1,63 @@
-const show = (value) =>{
-    let display = document.getElementById('display');
-    display.value += value;
-}
+let numbers = [];
+let operatorA = [];
+let number = '';
+const show = (value) => {
+  let display = document.getElementById('display');
+  display.value += value;
+  number+=value;
+};
 
-const clear = () => {
+const operator = (value) => {
+  operatorA.push(value);
+  let display = document.getElementById('display');
+  numbers.push(+display.value);
+  display.value += value;
+  number = '';
+};
+
+const clearC = () => {
     let display = document.getElementById('display');
+    numbers = [];
+    operatorA = [];
+    number = '';
     display.value = '';
-}
+};
 
 const calculate = () => {
-    let display = document.getElementById('display');
-    let result = eval(display.value);
-    display.value = result;
-}
+    numbers.push(+number);
+  let display = document.getElementById('display');
+
+
+  for (let i = 0; i < operatorA.length; i++) {
+    if (operatorA[i] == 'X') {
+      numbers[i] = numbers[i] * numbers[i + 1];
+      numbers.splice(i + 1, 1);
+      operatorA.splice(i, 1);
+      i--;
+    } else if (operatorA[i] == '/') {
+      if (numbers[i + 1] === 0) {
+        display.value = 'Syntax error';
+        return;
+      }
+      numbers[i] = numbers[i] / numbers[i + 1];
+      numbers.splice(i + 1, 1);
+      operatorA.splice(i, 1);
+      i--;
+    }
+  }
+  console.log(numbers);
+  let totalNumber = numbers[0];
+
+  for (let i = 0; i < operatorA.length; i++) {
+    if (operatorA[i] == '+') {
+      totalNumber += numbers[i + 1];
+    } else if (operatorA[i] == '-') {
+      totalNumber -= numbers[i + 1];
+    }
+  }
+
+  numbers = [];
+  operatorA = [];
+  number = '';
+  display.value = totalNumber.toString();
+};
